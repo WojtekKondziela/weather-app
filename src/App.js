e6c16ea440666b9b2c81eed291c24c7f77def546
Wojtek Kondziela <wojciech.kondziela@onet.pl>
2021-10-28 11:56:56 +0200

    refactor the entire app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Layout } from './components/Layout';
-import { ShowResponse } from './components/ShowResponse';
-import SunIcon from './assets/sun-icon2.png'
 
 
 class App extends Component {
 
-  state = {
-    isResponseReady: true
-  }
-
   render() {
 
-    if(this.state.isResponseReady === false) {
+    return(
 
-      return (
-
-        <React.Fragment>
-          <Layout>
-            <div className='animated-icon'>
-              <img src={SunIcon} alt='sun' className='spinning-icon'  />
-            </div>
-          </Layout>
-        </React.Fragment>
-  
-      );
-
-    } else if(this.state.isResponseReady === true) {
-
-      return (
-
-        <React.Fragment>
-          <Layout>
-            <ShowResponse />
-          </Layout>
-        </React.Fragment>
-
-      )
-
-    }
+      <h1>Hello World</h1>
+      
+    )
 
   }
-
 }
 
 export default App;
