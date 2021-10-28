47b617a6213c84fd25735b191e9c443a7ad33315
Wojtek Kondziela <wojciech.kondziela@onet.pl>
2021-10-28 20:41:43 +0200

    all data display properly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import './app.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWind, faCloud, faCalendarDay, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faWind, faCalendarDay, faMapMarkerAlt, faTint } from '@fortawesome/free-solid-svg-icons';
+import sunset from './assets/sunset.png';
+import sunrise from './assets/sunrise.png';
+import barometer from './assets/barometer.png';
 
 const API = {
 
@@ -14,53 +17,80 @@ const API = {
 
 function App() {
 
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({})
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({})
 
-    const search = evt => {
+  const search = e => {
 
-      if(evt.key === "Enter") {
+    if (e.key === "Enter") {
 
-        fetch(`${API.base}weather?q=${query}&units=${API.units}&APPID=${API.key}`)
-          .then(res => res.json())
-          .then(result => { 
+      fetch(`${API.base}weather?q=${query}&units=${API.units}&APPID=${API.key}`)
+        .then(res => res.json())
+        .then(result => {
 
-            setWeather(result)
-            setQuery('')
-            console.log(weather);
+          setWeather(result)
+          setQuery('')
+          console.log(weather);
 
-          });
-
-      }
+        });
 
     }
 
-    const getDate = (d) => {
+  }
 
-      let date = d.getDate();
-      let month = d.getMonth() + 1;
-      let year = d.getFullYear();
+  const getDate = (d) => {
 
-      return `${date}.${month}.${year}`;
+    let date = d.getDate();
+    let month = d.getMonth() + 1;
+    let year = d.getFullYear();
 
-    }
+    return `${date}.${month}.${year}`;
 
-    return(
+  }
 
-      <React.Fragment>
-        <Container>
-          <h1 className='app-main-heading text-center p-5'>Weather App</h1>
-          <div className='form w-100 text-center mb-5'>
-            <input className='findLocation text-center' type='text' placeholder='where we go now?' onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search} />
-          </div>
+  function convert_UNIX_sunset_to_date(sunset) {
+
+    let date = new Date(sunset * 1000);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let formatedMinutes = (minutes < 10) ? `0${minutes}` : `${minutes}`;
+
+    return `${hours}:${formatedMinutes}`;
+
+  }
+
+  function convert_UNIX_sunrise_to_date(sunrise) {
+
+    let date = new Date(sunrise * 1000);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let formatedMinutes = (minutes < 10) ? `0${minutes}` : `${minutes}`;
+
+    return `${hours}:${formatedMinutes}`;
+
+  }
+
+  return (
+
+    <React.Fragment>
+      <Container>
+        <h1 className='app-main-heading text-center p-5'>Weather App</h1>
+        <div className='form w-100 text-center mb-4'>
+          <input className='findLocation text-center' type='text' placeholder='where we go now?' onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search} />
+        </div>
+        {(typeof weather.main !== 'undefined') ? (
+
           <div className='weather-table pt-5'>
             <div className='temp item-1'>
-              <span className='weather-table__item p-3'>17&deg;C</span>
+              <span className='weather-table__item p-3'>
+                <span>{Math.round(weather.main.temp)}&deg;C</span>
+                <span className='temp_feels_like'>feels like {Math.round(weather.main.temp)}&deg;C</span>
+              </span>
             </div>
             <div className='city item-2'>
               <span className='weather-table__item p-3'>
                 <FontAwesomeIcon icon={faMapMarkerAlt} className='mx-3 icon' />
-                <span>Ustroń</span>
+                <span>{weather.name} <sup className='country'>{weather.sys.country}</sup></span>
               </span>
             </div>
             <div className='date item-3'>
@@ -71,22 +101,49 @@ function App() {
             </div>
             <div className='weather-icon item-4'>
               <span className='weather-table__item p-3'>
-                <FontAwesomeIcon icon={faCloud} className='mx-3 icon' />
-                <span>cloudy</span>
+                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt={weather.weather[0].description}  className='weather__img' />
+                <span>{weather.weather[0].description}</span>
               </span>
             </div>
             <div className='wind item-5'>
               <span className='weather-table__item p-3'>
                 <FontAwesomeIcon icon={faWind} className='mx-3 icon' />
-                <span>5 m/s</span>
+                <span>{weather.wind.speed} m/s</span>
+              </span>
+            </div>
+            <div className='wind item-6'>
+              <span className='weather-table__item p-3'>
+                <img src={sunrise} alt={weather.sys.sunrise} className='icon' />
+                <span>{convert_UNIX_sunrise_to_date(weather.sys.sunrise)}</span>
+              </span>
+            </div>
+            <div className='wind item-7'>
+              <span className='weather-table__item p-3'>
+                <img src={barometer} alt={weather.main.pressure} className='icon' />
+                <span>{weather.main.pressure} hPa</span>
+              </span>
+            </div>
+            <div className='wind item-8'>
+              <span className='weather-table__item p-3'>
+                <FontAwesomeIcon icon={faTint} className='mx-3 icon' />
+                <span>{weather.main.humidity} %</span>
+              </span>
+            </div>
+            <div className='wind item-9'>
+              <span className='weather-table__item p-3'>
+                <img src={sunset} alt={weather.sys.sunset} className='icon' />
+                <span>{convert_UNIX_sunset_to_date(weather.sys.sunset)}</span>
               </span>
             </div>
           </div>
+
+        ) : ('')}
+
         </Container>
-      </React.Fragment>
+    </React.Fragment>
 
-    )
+  )
 
-  }
+}
 
 export default App;
